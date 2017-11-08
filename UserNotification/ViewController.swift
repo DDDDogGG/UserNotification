e852a5366b375a1b14b13224f99cd0f9e2356320
JiangLun <jiangluntc@126.com>
2017-11-08 14:11:44 +0800

    Initial commit

---
+++ UserNotification/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotification
+//
+//  Created by 姜伦 on 2017/11/8.
+//  Copyright © 2017年 Django. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
