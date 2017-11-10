e78d5715fda2fa0291d44c0ec396c610b5e3b9cf
JiangLun <jiangluntc@126.com>
2017-11-10 11:04:48 +0800

    处理远程通知的另一个方法

--- UserNotification/AppDelegate.swift
+++ UserNotification/AppDelegate.swift
@@ -110,6 +110,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
         // Else handle actions for other notification types...
     }
+    
+    // Handle notifications in your app delegate (maybe another way -> User Notification framework)
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // Don't kown the [completionHandler] mean
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
